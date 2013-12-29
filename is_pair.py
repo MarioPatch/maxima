fdfc1b50e738f0256ae76c615ef3fe65989b24a1
Mario Patch <mario.patch@gmail.com>
2013-12-29 10:23:32 +0100

    apprentissage langage Maxima

---
+++ is_pair.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+ma_var=19
+if ma_var%2==0:
+  print("c'est pair") 
+else: 
+    print("c'est pas pair")
+
+
+
---
+++ mageur_mineur.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+mon_age="dscjspe"
+if isinstance(mon_age,int):
+    if mon_age>18:
+        print("tu es majeur")
+    elif mon_age==18:
+        print("t'es nul")
+    else:
+        print("tu es mineur")
+else:
+     print("variable incorrect")
+
+
+
